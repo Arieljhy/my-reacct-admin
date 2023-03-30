43a38f5fa64162a14fcdd1960ccb53a0a25f372e
arieljhy <ariel_jhy@163.com>
2023-03-30 13:50:08 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,20 @@ import enUS from "antd/lib/locale/en_US";
 import i18n from "i18next";
 import "moment/dist/locale/zh-cn";
 
+//q: 插入排序
+//a: 从第一个元素开始，该元素可以认为已经被排序；取出下一个元素，在已经排序的元素序列中从后向前扫描；如果该元素（已排序）大于新元素，将该元素移到下一位置；重复步骤3，直到找到已排序的元素小于或者等于新元素的位置；将新元素插入到该位置后；重复步骤2~5。
+const insertionSort = (arr: number[]) => {
+	for (let i = 1; i < arr.length; i++) {
+		let j = i - 1;
+		let temp = arr[i];
+		while (j >= 0 && arr[j] > temp) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
+	}
+	return arr;
+};
 const App = (props: any) => {
 	const { language, assemblySize, themeConfig, setLanguage } = props;
 	const [i18nLocale, setI18nLocale] = useState(zhCN);
